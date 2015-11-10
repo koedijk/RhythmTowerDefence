937933134df3cc682c0db6432ac6cf81bf491aea
Ralph Otte <officialblaszix@gmail.com>
2015-11-10 20:35:44 +0100

    Minor changes to Spawners, Objects and a Destroyer
    -Fixed some issues with BPM/BPS calculation
    -Begun on a Destroyer for missed Notes
    -Changed some spawn rates to more currently correct values

--- Proof of Concept 2D Tower Defense/Assets/Scripts/EnemyMovement.cs
+++ Proof of Concept 2D Tower Defense/Assets/Scripts/EnemyMovement.cs
@@ -2,11 +2,11 @@
 using System.Collections;
 
 public class EnemyMovement : MonoBehaviour {
-	public float scrollSpeed;
+	public float walkSpeed;
 
 	// Update is called once per frame
 	void Update () {
-		transform.position -= new Vector3 (scrollSpeed * Time.deltaTime, 0.0f, 0.0f);
+		transform.position -= new Vector3 (walkSpeed * Time.deltaTime, 0.0f, 0.0f);
 
 
 	//	if (this.transform.position.x >= 0 /*Voor de toren*/) {				
--- Proof of Concept 2D Tower Defense/Assets/Scripts/NoteSpawner.cs
+++ Proof of Concept 2D Tower Defense/Assets/Scripts/NoteSpawner.cs
@@ -4,22 +4,30 @@ using System.Collections;
 public class NoteSpawner : MonoBehaviour {
 
 	public GameObject[] obj;
-	public float BPM = 180; //BPM
-	public float BPM2 = 160; //BPM 2
+	[SerializeField]
+	private float BPM1 = 180; //BPM nummer 1
+	[SerializeField]
+	private float BPM2 = 200; //BPM nummer 2
 
-	//public float BPS1 = BPM / 60; // 180 / 60 = 3 in dit geval, Er worden dus 3 monsters per seconde gespawnd
-	//public float BPS2 = BPM2 / 60; // 160/ 60 = 2.666 in dit geval, Er worden dus 2.66 monsters per seconde gespawnd
+	[SerializeField]
+	private float BPS1; 
+	[SerializeField]
+	private float BPS2; 
 
+	//Nieuwe BPS in de levels/waves inbouwen met "ActivateBPS" functies
 
 	// Use this for initialization
 	void Start () {
 		Spawn (); //Voert functie Spawn uit
+		BPS1 = BPM1 / 60 / 5; //Rekent het BPS uit voor BPS1, 180 / 60 = 3 / 5 = 0.6 in dit geval, Er worden dus minimaal 0.6 Notes per seconde gespawnd
+		BPS2 = BPM2 / 60 / 5; //Rekent het BPS uit voor BPS2, 200 / 60 = 3.333... / 5 = 0.6 in dit geval, Er worden dus maximaal 0.666... Notes per seconde gespawnd 
 	}
 	
 	void Spawn()
 	{
-		Instantiate(obj[Random.Range (0, obj.GetLength(0))], transform.position, Quaternion.identity); //Spawnt 1 van de objecten in de array "obj"
-		Invoke ("Spawn", Random.Range (BPM, BPM2)); //Voert deze functie opnieuw uit met de gegeven parameters, in dit geval blijven die altijd hetzelfde.
+		Instantiate(obj[Random.Range (0, obj.GetLength(0))], this.transform.position, Quaternion.identity); //Spawnt 1 van de objecten in de array "obj" op de plaats van de spawner
+
+		Invoke ("Spawn", Random.Range (BPS1, BPS2)); //Voert deze functie opnieuw uit met de gegeven parameters, in dit geval blijven die altijd hetzelfde.
 		//BPS1 is minimum random BPS, BPS2 is maximum random BPS, deze moet nog omgebouwd worden naar 1 constante snelheid afhankelijk van welk punt in het nummer wordt afgespeeld
 	}
 }
