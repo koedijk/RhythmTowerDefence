a46b81c16ab43d5ba17065540e9a452f5078be12
Koedijk <17798@ma-web.nl>
2015-11-26 12:53:30 +0100

    Commit Ralph
    stuff

---
+++ Proof of Concept 2D Tower Defense/Assets/Scripts/Level&SpawnData/SpawnNewWave.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnNewWave : MonoBehaviour {
+	public GameObject[] obj;
+	private float SpawnRateMin = 3;
+	private float SpawnRateMax = 5;
+	public float newSpawnRate;
+
+	private int waveIndex = 0;
+	private int spawnNew = 0;
+
+	public void Spawn()
+	{
+		waveIndex+= 3;
+		         
+
+		for (int i = 0; i < waveIndex; i++) {
+			Instantiate (obj [Random.Range (0, obj.GetLength (0))], this.transform.position, Quaternion.identity);
+			Debug.Log (i);
+			//spawnNew--;
+		}
+	//	Invoke("Spawn", newSpawnRate);
+	}
+}
